ff28864f1a46eca020e519990a6e186ae821ce73
Samuel Galanakis <samme.galanakis6@gmail.com>
2026-07-10 16:20:55 +0200

    Respect Android status bar insets

--- android/app/src/main/kotlin/dev/hirsel/android/MainActivity.kt
+++ android/app/src/main/kotlin/dev/hirsel/android/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -110,6 +111,7 @@ private fun HirselApp() {
     Surface(
         modifier = Modifier
             .fillMaxSize()
+            .statusBarsPadding()
             .semantics { testTagsAsResourceId = true },
         color = Color(0xFFF7F7F2),
     ) {

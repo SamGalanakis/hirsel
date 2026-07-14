e616176de4e099d11e0a893c2b623774a03ed12f
Samuel Galanakis <samme.galanakis6@gmail.com>
2026-07-15 00:19:08 +0200

    Point ping-read runbook at the registry model

--- e2e/ping-read/run.sh
+++ e2e/ping-read/run.sh
@@ -12,7 +12,7 @@ HOST_LOG="/tmp/hirsel-e2e-ping-read-host.log"
 HIRSEL_AGENT=scripted
 HIRSEL_PROVIDER=codex
 HIRSEL_DRIVER=fake
-HIRSEL_MODEL=gpt-5.5
+HIRSEL_MODEL=gpt-5.6-sol
 trap 'stop_hirsel_host TERM' EXIT
 
 start_hirsel_host fresh
